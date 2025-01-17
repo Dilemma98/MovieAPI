409948605c8279fd9325dafbc3e9e059262b90c7
jehad abdoo <jood111ao@hotmail.com>
2025-01-17 10:52:51 +0100

    credits och options

--- script.js
+++ script.js
@@ -1,59 +1,73 @@
 let movieList = document.getElementById("movieList");
 let movieInfo = document.getElementById("movieInfo");
-
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4OGQ2ZjkwNmIzODZhYzQ3YzAwNDcwMWQ4ZjU0NWRmOCIsIm5iZiI6MTcwNDM2MjAwNC4zODksInN1YiI6IjY1OTY4MDE0ZWEzN2UwMDZmYTRjYWQ4YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fAIGy5BaC3YiG8Y8WMLb3GSnG9eSm4h4OKMbQHC-pu0",
+  },
+};
 getMovieList();
+
+function getCredits(id) {
+  fetch(`https://api.themoviedb.org/3/movie/${id}/credits`, options)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.cast);
+      printCreditsList(data.cast);
+    });
+}
 
 function getMovieList() {
   fetch(
     "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc",
-    {
-      method: "GET",
-      headers: {
-        //accept: "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4OGQ2ZjkwNmIzODZhYzQ3YzAwNDcwMWQ4ZjU0NWRmOCIsIm5iZiI6MTcwNDM2MjAwNC4zODksInN1YiI6IjY1OTY4MDE0ZWEzN2UwMDZmYTRjYWQ4YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fAIGy5BaC3YiG8Y8WMLb3GSnG9eSm4h4OKMbQHC-pu0",
-      },
-    }
+    options
   )
     .then((res) => res.json())
     .then((data) => {
-        
-        printMovieList(data.results);
-        
+      printMovieList(data.results);
+      console.log(data.results);
     });
 }
+function printCreditsList(id) {
+  id.map((name) => {
+    let li = document.createElement("p");
+    li.innerText = name.name;
+    movieInfo.append(li);
+  });
+}
 
-function printMovieList(movies){
-    movies.map(movie => {
-    
+function printMovieList(movies) {
+  movies.map((movie) => {
+    let li = document.createElement("li");
+    li.innerText = movie.original_title;
 
-        let li = document.createElement("li");
-        li.innerText = movie.original_title;
+    li.addEventListener("click", () => {
+      console.log("click på film", movie.original_title);
+      printMovieDetails(movie);
+    });
 
-        li.addEventListener("click", () => {
-            console.log("click på film", movie.original_title);
-            printMovieDetails(movie);
-        })
-
-        movieList.appendChild(li);
-    })
+    movieList.appendChild(li);
+  });
 }
 
 function printMovieDetails(movie) {
-    let h3 = document.createElement("h3");
-    h3.innerText = movie.original_title;
+  movieInfo.innerText = "";
+  let h3 = document.createElement("h3");
+  h3.innerText = movie.original_title;
 
-    let p = document.createElement("p");
-    p.innerText = movie.overview;
+  let p = document.createElement("p");
+  p.innerText = movie.overview;
 
-    let img = document.createElement("img");
-    img.src = "http://image.tmdb.org/t/p/w500" + movie.poster_path;
+  let img = document.createElement("img");
+  img.src = "http://image.tmdb.org/t/p/w500" + movie.poster_path;
 
-    let b = document.createElement("b");
-    b.innerText = 
-    "Rating: " + movie.vote_average + " \n" +
-    "Votes: " + movie.vote_count;
+  let b = document.createElement("b");
+  b.innerText =
+    "Rating: " + movie.vote_average + " \n" + "Votes: " + movie.vote_count;
 
-    movieInfo.innerHTML = "";
-    movieInfo.append(h3, img, p, b);
+  getCredits(movie.id);
+
+  movieInfo.append(h3, img, p, b);
 }
